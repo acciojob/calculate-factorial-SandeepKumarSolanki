345f8eb229e3a703b4a89d04d92897ece0692c78
SandeepKumarSolanki <166508938+SandeepKumarSolanki@users.noreply.github.com>
2025-05-09 16:48:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+
+function factorial(){
+	let a = prompt("Enter a number");
+	a = +a;
+	let f = 1;
+	for(let i=1 ; i<=a ; i++){
+		f *= i;
+	}
+	alert(`The factorial of ${a} is ${f}`)
+}
+
+factorial()
